1e3851b63ba7ee4b2ea98bc6744d66799f70fc16
KadyntsevAN <60112610+KadyntsevAN@users.noreply.github.com>
2024-01-17 02:00:54 +0300

    1.1.2. JDBC

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,23 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.model.User;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
+        userDaoJDBC.createUsersTable();
+        userDaoJDBC.saveUser("Ivan", "Petrov", (byte) 42);
+        userDaoJDBC.saveUser("Petr", "Sidorov", (byte) 34);
+        userDaoJDBC.saveUser("Olga", "Popova", (byte) 51);
+        userDaoJDBC.saveUser("Vladimir", "Smirnov", (byte) 23);
+        List<User> list = userDaoJDBC.getAllUsers();
+        for (User user : list) {
+            System.out.println(user);
+        }
+        userDaoJDBC.cleanUsersTable();
+        userDaoJDBC.dropUsersTable();
     }
 }
